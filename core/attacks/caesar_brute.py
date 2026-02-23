7382ef79e35716d56630f190125b943cfa8ecec6
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-02-23 14:10:54 +0530

    Update caesar_brute.py

--- core/attacks/caesar_brute.py
+++ core/attacks/caesar_brute.py
@@ -1,1 +1,27 @@
+def brute_force(ciphertext: str) -> list:
+    results = []
 
+    for shift in range(1, 26):
+        left_result = ""
+        right_result = ""
+
+        for char in ciphertext:
+            if char.isalpha():
+                base = ord("A") if char.isupper() else ord("a")
+
+                # Left shifting (decrypting)
+                left_result += chr((ord(char) - base - shift) % 26 + base)
+
+                # Right shifting (encrypting)
+                right_result += chr((ord(char) - base + shift) % 26 + base)
+            else:
+                left_result += char
+                right_result += char
+
+        results.append({"shift": shift, "direction": "left", "plaintext": left_result})
+
+        results.append(
+            {"shift": shift, "direction": "right", "plaintext": right_result}
+        )
+
+    return results
