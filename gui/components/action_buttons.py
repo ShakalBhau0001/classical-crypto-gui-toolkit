35be224b4f10cd00fa48aaca249dd4c6a4190eb0
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-02-26 10:36:22 +0530

    Update action_buttons.py

--- gui/components/action_buttons.py
+++ gui/components/action_buttons.py
@@ -1,1 +1,16 @@
+import customtkinter as ctk
 
+
+class ActionButtons(ctk.CTkFrame):
+    def __init__(self, parent, encrypt_callback, decrypt_callback):
+        super().__init__(parent)
+
+        self.encrypt_btn = ctk.CTkButton(
+            self, text="Encrypt", command=encrypt_callback, width=120
+        )
+        self.encrypt_btn.pack(side="left", padx=10)
+
+        self.decrypt_btn = ctk.CTkButton(
+            self, text="Decrypt", command=decrypt_callback, width=120
+        )
+        self.decrypt_btn.pack(side="left", padx=10)
