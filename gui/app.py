dbe65218b48ee2d887299cc0fc7605058dab2e4e
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-02-28 00:22:52 +0530

    Update app.py

--- gui/app.py
+++ gui/app.py
@@ -1,1 +1,10 @@
+import customtkinter as ctk
+from .main_window import MainWindow
 
+
+def run_app():
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
+
+    app = MainWindow()
+    app.mainloop()
