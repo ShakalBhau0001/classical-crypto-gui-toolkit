8ddd9d99f58a63856eb329948a9ed2b1b4c5e83e
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-02-28 00:24:14 +0530

    Update validators.py

--- gui/validators.py
+++ gui/validators.py
@@ -1,1 +1,13 @@
+def validate_int(value: str):
+    if not value.isdigit():
+        raise ValueError("Key must be numeric.")
+    return int(value)
 
+
+def validate_string(value: str):
+    value = value.strip()
+    if not value:
+        raise ValueError("Key cannot be empty.")
+    if len(set(value)) != len(value):
+        raise ValueError("Duplicate characters not allowed in key.")
+    return value
